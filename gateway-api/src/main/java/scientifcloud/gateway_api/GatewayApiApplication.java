cf404c377eec31be95afa02e7093f97c08c522b2
sc-leticia <leticia@s-c.ai>
2025-09-11 17:46:43 -0300

    feat: add initial configuration of spring cloud gateway with java in application.yaml file, needs additional config and testing

---
+++ gateway-api/src/main/java/scientifcloud/gateway_api/GatewayApiApplication.java
@@ -0,0 +1,13 @@
+package scientifcloud.gateway_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApiApplication.class, args);
+	}
+
+}
